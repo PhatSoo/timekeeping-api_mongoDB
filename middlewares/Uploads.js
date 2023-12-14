72429c315e329ea5da8da1df7b204b175776c976
PhatSoo <phat.ltrung+github@gmail.com>
2023-12-14 10:25:44 +0700

    upload middleware

--- middlewares/Uploads.js
+++ middlewares/Uploads.js
@@ -2,32 +2,33 @@ const multer = require('multer');
 const path = require('path');
 
 // Storage for local
-const storageAvatar = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads');
-  },
-  filename: (req, file, cb) => {
-    const { _id } = req.body;
+// const storageAvatar = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, './uploads');
+//   },
+//   filename: (req, file, cb) => {
+//     const { _id } = req.body;
 
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    const extension = path.extname(file.originalname);
-    cb(null, 'AV-' + _id + '-' + uniqueSuffix + extension);
-  },
-});
+//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+//     const extension = path.extname(file.originalname);
+//     cb(null, 'AV-' + _id + '-' + uniqueSuffix + extension);
+//   },
+// });
 
-const storageAttendance = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads');
-  },
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    const extension = path.extname(file.originalname);
-    cb(null, 'AT-' + req.userId + '-' + uniqueSuffix + extension);
-  },
-});
+// const storageAttendance = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, './uploads');
+//   },
+//   filename: (req, file, cb) => {
+//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+//     const extension = path.extname(file.originalname);
+//     cb(null, 'AT-' + req.userId + '-' + uniqueSuffix + extension);
+//   },
+// });
 
 const uploadAvatar = multer({
-  storage: storageAvatar,
+  // storage: storageAvatar,
+  storage: multer.memoryStorage(),
   limits: { fieldSize: 10000000 /* 10MB */ },
   fileFilter: (req, file, cb) => {
     if (file.mimetype == 'image/png' || file.mimetype == 'image/jpeg') {
@@ -43,7 +44,8 @@ const uploadAvatar = multer({
 });
 
 const uploadAttendance = multer({
-  storage: storageAttendance,
+  // storage: storageAttendance,
+  storage: multer.memoryStorage(),
   limits: { fieldSize: 10000000 /* 10MB */ },
   fileFilter: (req, file, cb) => {
     if (file.mimetype == 'image/png' || file.mimetype == 'image/jpeg') {
